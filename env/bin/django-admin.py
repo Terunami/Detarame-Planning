06212e052cba65eb284000a416ae27e22801edb0
Terunami <nobooknolife16@yahoo.co.jp>
2020-07-03 01:03:41 +0900

    django start

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/oobatakahiro/Desktop/python_work/Detarame-Planning/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
